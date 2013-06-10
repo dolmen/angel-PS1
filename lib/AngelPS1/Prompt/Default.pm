025e62769bac634ae4f54ac4328d39cc92aa4a33
Olivier Mengué <dolmen@cpan.org>
2013-06-10 23:41:21 +0200

    Extract the default prompt as a module: AngelPS1/Prompt/Default.pm
    I would have prefered to name it AngelPS1/Prompt/Default.pl.
    Unfortunately FatPack does not accepts .pl files.

---
+++ lib/AngelPS1/Prompt/Default.pm
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+
+package AngelPS1::Prompt::Default;
+
+use AngelPS1::Color;
+use AngelPS1::Plugin::Core;
+use AngelPS1::Plugin::TerminalSize;
+use AngelPS1::Plugin::Git;
+
+use POSIX ();
+
+(my $TTYNAME = POSIX::ttyname(0)) =~ s{^/dev/}{};
+
+# The prompt is the list returned as the last statement
+(
+    \$BLUE,
+    sub { sprintf('%3$02d:%2$02d:%1$02d', localtime) },
+    ' ',
+    $TTYNAME,
+    (sub { "(${COLUMNS}x${LINES})" }) x!! $AngelPS1::DEBUG,
+    ' ',
+    sub { -w $_[0]->{PWD} ? (\$GREEN) : (\$RED) },
+    ':',
+    '\\w',
+    ' ',
+    \&GitInfo,
+    sub { my $err = $_[0]->{'?'}; $err == 0 ? () : (\$RED, $err, ' ') },
+    # User mark: root => #    else  $
+    ($< ? (\$BOLD, '\\$') : (\"$BOLD$RED", '#')),
+    ' ',
+)
+# vim:set et ts=8 sw=4 sts=4:
