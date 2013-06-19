76899cbba7d36ba77128d6ac6902ed65dbf78bc1
Olivier Mengué <dolmen@cpan.org>
2013-06-20 00:37:39 +0200

    AngelPS1::Shell: tabs 2 spaces

--- lib/AngelPS1/Shell.pm
+++ lib/AngelPS1/Shell.pm
@@ -25,16 +25,16 @@ sub use
     my ($class, $shell) = @_;
     if ($shell) {
         $shell = $ALIASES{$shell} if exists $ALIASES{$shell};
-	my $src = "AngelPS1/Shell/$shell.pm";
-	unless (exists $INC{$src}) {
-	    # TODO try to distinguish load errors (file not found) from compile errors
-	    # by pushing a sub on @INC that will be called.
-	    die "$shell is not a supported shell.\n" unless eval { require $src };
-	    my $pkg = "${class}::$shell";
-	    # Make AngelPS1::Shell a sub class of $pkg
-	    our @ISA = ($pkg);
-	    $name = $shell;
-	}
+        my $src = "AngelPS1/Shell/$shell.pm";
+        unless (exists $INC{$src}) {
+            # TODO try to distinguish load errors (file not found) from compile errors
+            # by pushing a sub on @INC that will be called.
+            die "$shell is not a supported shell.\n" unless eval { require $src };
+            my $pkg = "${class}::$shell";
+            # Make AngelPS1::Shell a sub class of $pkg
+            our @ISA = ($pkg);
+            $name = $shell;
+        }
     }
 }
 
