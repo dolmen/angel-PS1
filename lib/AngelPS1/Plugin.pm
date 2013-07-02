447a4d6af1cb476090ce1a2e897b0e0ad6ea6a60
Olivier Mengué <dolmen@cpan.org>
2013-07-02 22:44:58 +0200

    AngelPS1::Plugin: cleanup to almost empty

--- lib/AngelPS1/Plugin.pm
+++ lib/AngelPS1/Plugin.pm
@@ -3,13 +3,7 @@ use warnings;
 
 package AngelPS1::Plugin;
 
-our $VERSION = $AngelPS1::VERSION;
-
-use Exporter 5.57 'import';
-our @EXPORT = qw(compact interp);
-
-*compact = \&AngelPS1::compact;
-*interp = \&AngelPS1::interp;
+#our $VERSION = $AngelPS1::VERSION;
 
 '$';
 # vim:set et ts=8 sw=4 sts=4:
