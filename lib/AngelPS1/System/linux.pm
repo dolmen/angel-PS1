a52dc08b9f27ec2947affe3ab4fe035387c87bbe
Olivier Mengué <dolmen@cpan.org>
2013-07-25 23:41:36 +0200

    System::linux: add 'nproc'

--- lib/AngelPS1/System/linux.pm
+++ lib/AngelPS1/System/linux.pm
@@ -3,6 +3,17 @@ use warnings;
 
 package AngelPS1::System::linux;
 
+sub nproc
+{
+    # Alternative: grep '^processor\t' /proc/cpuinfo
+
+    my $nproc = `nproc`;
+    return if $? < 0;
+    return if $? & 127;
+    chomp $nproc;
+    $nproc
+}
+
 sub loadavg
 {
     open my $proc_loadavg, '<', '/proc/loadavg' or die;
