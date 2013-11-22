1cd3b2b940be310a8741ae2304a0f75ced8de154
Olivier Mengué <dolmen@cpan.org>
2013-11-22 01:34:50 +0100

    Compiler: refactoring for improved diagnostics

--- lib/AngelPS1/Compiler.pm
+++ lib/AngelPS1/Compiler.pm
@@ -47,20 +47,10 @@ sub reduce
     my @out;
     while (@template) {
         my $v = shift @template;
-        my $r = ref $v;
-        if ($r) {
-            # Keep subs as they must be explicitely expanded using expand()
-            if ($r eq 'CODE') {
-                push @out, $v;
-                next;
+        if (my $r = ref $v) {
             # Scalar refs are for raw (non-escaped) strings
-            } elsif ($r eq 'SCALAR') {
+            if ($r eq 'SCALAR') {
                 $v = $$v;
-            # Array refs are only expanded after a chrome spec. See below
-            } elsif ($r eq 'ARRAY') {
-                warn "ARRAY unexpected in reduced prompt\n" unless wantarray;
-                push @out, [ reduce(@$v) ];
-                next;
             }
             # => replace by the colored expanded result
             elsif (Scalar::Util::blessed($v) && $v->isa('AngelPS1::Chrome')) {
@@ -69,6 +59,7 @@ sub reduce
                         AngelPS1::Shell->ps1_invisible("$v"),
                         # flatten the ARRAY
                         @{ shift @template },
+                        # close the colored part with a Reset
                         AngelPS1::Shell->ps1_invisible(AngelPS1::Chrome::Reset->term);
                 } else {
                     # Expand the color
@@ -76,7 +67,18 @@ sub reduce
                 }
                 redo;
             } else {
-                warn "unexpected $r item in prompt";
+                if (wantarray) {
+                    # Keep subs as they must be explicitely expanded using expand()
+                    if ($r eq 'CODE') {
+                        push @out, $v;
+                        next;
+                    # Array refs are only expanded after a chrome spec. See below
+                    } elsif ($r eq 'ARRAY') {
+                        push @out, [ reduce(@$v) ];
+                        next;
+                    }
+                }
+                warn "unexpected $r item in prompt\n";
                 next;
             }
         } else {
@@ -90,7 +92,7 @@ sub reduce
     }
     return @out if wantarray;
     return '' unless @out;
-    die "invalid state after reduce: @out" if @out != 1 || ref $out[0] ne 'SCALAR';
+    die "invalid state after reduce: @out\n" if @out != 1 || ref $out[0] ne 'SCALAR';
     ${pop @out}
 }
 
