3d52cc144c4601407a58b97bb030f16d6960534d
Olivier Mengué <dolmen@cpan.org>
2013-12-06 20:18:00 +0100

    Add AngelPS1::Plugin::VCS

---
+++ lib/AngelPS1/Plugin/VCS.pm
@@ -0,0 +1,60 @@
+use strict;
+use warnings;
+
+package AngelPS1::Plugin::VCS;
+
+use Exporter 'import';
+our @EXPORT = qw<VCSInfo>;
+
+sub _find_vcs_dir
+{
+    my $dir = shift;
+    my @stat = stat $dir;
+    my $dev = $stat[0];
+    while ($dir && $stat[0] == $dev) {
+        if ($stat[3] > 3) {
+            return (git => $dir) if -d "$dir/.git/objects";
+            return (svn => $dir) if -f "$dir/.svn/entries";
+            return (hg  => $dir) if -d "$dir/.hg/store";
+            return (bzr => $dir) if -d "$dir/.bzr";
+            return ('git-bare' => $dir) if substr($dir, -4) eq '.git' && -d "$dir/objects"
+        }
+        # go up
+        substr($dir, 0, rindex($dir, '/'), '');
+        @stat = stat $dir;
+    }
+    return
+}
+
+my %VCS = (
+    git => [
+        'AngelPS1/Plugin/Git.pm',
+        sub {
+            my ($state, $dir) = @_;
+            $state->{GIT_DIR} = $dir;
+            my @res = AngelPS1::Plugin::Git::GitInfo($state);
+            delete $state->{GIT_DIR};
+            @res
+        }
+    ],
+);
+
+sub VCSInfo
+{
+    my $options = (@_ && ref($_[0]) eq 'HASH') ? (shift) : {};
+    my @enabled_vcs = @_;
+
+    sub {
+        my $state = shift;
+        my ($vcs, $dir) = _find_vcs_dir($state->{PWD});
+        my $vcs_plugin = $VCS{$vcs};
+        return unless defined $vcs_plugin;
+
+        require $vcs_plugin->[0];
+        return $vcs_plugin->[1]->($state, $dir)
+    }
+}
+
+1;
+__END__
+# vim:set et ts=8 sw=4 sts=4:
