463645db82485b154e2c68fb21725ae0f062c7e0
Olivier Mengué <dolmen@cpan.org>
2013-12-08 21:44:56 +0100

    demo: many script improvements

--- demo.sh
+++ demo.sh
@@ -9,14 +9,21 @@ ln -s "$PWD" "$tmp/angel-PS1"
 out="$tmp/out"
 
 unset PS1
-( HOME="$tmp" ; cd ~/angel-PS1 ; script -q -e -c 'bash -l' <<'EOF' > "$out" )
-echo -e '# == Loading with default prompt ====='
-echo 'eval $(./angel-PS1)' ; eval $(./angel-PS1)
-echo -e '# == Loading with 2lines.PS1 '
-echo 'eval $(./angel-PS1 -c examples/2lines.PS1)' ; eval $(./angel-PS1 -c examples/2lines.PS1)
-echo -e '# == Loading with Powerline-basic.PS1'
-echo 'eval $(./angel-PS1 -c examples/Powerline-basic.PS1)' ; eval $(./angel-PS1 -c examples/Powerline-basic.PS1)
-echo exit
+HOME="$tmp"
+cd ~/angel-PS1 ; script -q -e -c 'bash -l' "$out" <<'EOF'
+echo ; stty echo
+echo $PS1
+# == Load angel-PS1 with default prompt =====
+eval $(./angel-PS1)
+angel off
+angel on
+angel quit
+# == Load angel-PS1 with Powerline-basic.PS1
+eval $(./angel-PS1 -c examples/Powerline-basic.PS1)
+angel quit
+set | grep ^APS1
+# == Load angel-PS1 with 2lines.PS1
+eval $(./angel-PS1 -c examples/2lines.PS1)
 exit
 EOF
 
