af5f1da915f06eab9435699e6b1b60bf39ad2258
Olivier Mengué <dolmen@cpan.org>
2013-12-09 00:19:54 +0100

    demo: more technicals details shown (too much?)

--- demo.sh
+++ demo.sh
@@ -15,18 +15,26 @@ unset PS1
 HOME="$tmp"
 cd ~/angel-PS1 ; script -q -e -c 'bash -l' "$out" <<'EOF'
 echo ; stty echo
-echo $PS1
+echo "'$PS1'"
 # == Load angel-PS1 with default prompt =====
 eval $(./angel-PS1)
 angel off
 angel on
 angel quit
+#
 # == Load angel-PS1 with Powerline-basic.PS1
+# (requires install of Powerline fonts)
 eval $(./angel-PS1 -c examples/Powerline-basic.PS1)
+set | grep ^APS1
+ps
 angel quit
 set | grep ^APS1
+#
 # == Load angel-PS1 with 2lines.PS1
 eval $(./angel-PS1 -c examples/2lines.PS1)
+# This is a static prompt
+echo $PROMPT_COMMAND
+printf '%q\n' "$PS1"
 exit
 EOF
 
