30778dc30827fd0ad9fc0e89a0822e313e436588
Olivier Mengué <dolmen@cpan.org>
2014-01-24 00:49:33 +0100

    Add BatteryGauge plugin
    Show the battery level as a Unicode gauge: from '_' (almost empty) to
    '█' (U+2588, full) with 7 levels between.

--- lib/AngelPS1/Plugin/Battery.pm
+++ lib/AngelPS1/Plugin/Battery.pm
@@ -8,7 +8,7 @@ package AngelPS1::Plugin::Battery;
 our $VERSION = $AngelPS1::VERSION;
 
 use Exporter 5.57 'import';
-our @EXPORT = qw(BatteryPercent);
+our @EXPORT = qw<BatteryPercent BatteryGauge>;
 
 use AngelPS1::Shell;
 use AngelPS1::System;
@@ -39,6 +39,32 @@ sub BatteryPercent
 				: $SYMBOL_DISCHARGING),
 		    100 * $status[0]
 	    ]
+	)
+    }
+}
+
+sub BatteryGauge
+{
+    return unless AngelPS1::System->can('fetch_battery');
+    my $fetch_battery = AngelPS1::System->fetch_battery
+	or return;
+
+    require AngelPS1::Plugin::Gauges;
+
+    return sub {
+	my @status = $fetch_battery->();
+	return unless @status;
+	(
+	    (
+		$status[0] > 0.20
+		? Green
+		: Red
+	    ),
+	    [
+		($status[1] ? $SYMBOL_CHARGING
+			    : $SYMBOL_DISCHARGING),
+		&AngelPS1::Plugin::Gauges::CharGauge($status[0])
+	    ],
 	)
     }
 }
