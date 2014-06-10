d76b0b34a6d8b79011aa87ac279446dc58277fe9
Olivier Mengué <dolmen@cpan.org>
2014-06-11 01:27:30 +0200

    Default prompt: use AngelPS1::Shell imports

--- lib/AngelPS1/Prompt/Default.pm
+++ lib/AngelPS1/Prompt/Default.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package AngelPS1::Prompt::Default;
 
-use AngelPS1::Shell ();
+use AngelPS1::Shell qw< WorkingDir_Tilde UserPrivSymbol >;
 use AngelPS1::Chrome;
 use AngelPS1::Plugin::Core 'MarginLeft';
 use AngelPS1::Plugin::DateTime;
@@ -35,7 +35,7 @@ return () unless AngelPS1::Shell->can('WorkingDir_Tilde')
                 # Columns and lines are dynamic!
                 sub { "${COLUMNS}x${LINES}) " },
             ) : ()),
-            AngelPS1::Shell->WorkingDir_Tilde,
+            WorkingDir_Tilde,
         )
     ),
     # fish has its own special handling through the fish_title function
@@ -46,12 +46,12 @@ return () unless AngelPS1::Shell->can('WorkingDir_Tilde')
     # User name
     $< ? (scalar getpwuid $<) : (),
     sub { ((-w $_[0]->{PWD} ? Green : Red), [ ':' ]) },
-    AngelPS1::Shell->WorkingDir_Tilde,
+    WorkingDir_Tilde,
     ' ',
     VCSInfo,
     sub { my $err = $_[0]->{'?'}; $err == 0 ? () : (Red, [ $err ], ' ') },
     # User mark: root => #    else  $
-    ($< ? (Bold, [ AngelPS1::Shell->UserPrivSymbol ]) : (Red + Bold, [ '#' ])),
+    ($< ? (Bold, [ UserPrivSymbol ]) : (Red + Bold, [ '#' ])),
     ' ',
 )
 # vim:set et ts=8 sw=4 sts=4:
