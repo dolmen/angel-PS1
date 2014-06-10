b38a30e19a6a3817d5f582aaf6613764d54c75a2
Olivier Mengué <dolmen@cpan.org>
2014-06-10 09:38:33 +0200

    Battery/linux: fix fetch for recent kernels (Ubuntu 14.04 : 3.13.0)

--- lib/AngelPS1/System/linux.pm
+++ lib/AngelPS1/System/linux.pm
@@ -53,6 +53,14 @@ sub gen_fetch_battery
     my $bat = shift @batteries;
 
     # Sub that will query the battery status
+    if (-f "$bat/capacity") {
+        return sub {
+            defined(my $capacity = _slurp_line "$bat/capacity") or return;
+            defined(my $status = _slurp_line "$bat/status") or return;
+            # 'Charging', 'Discharging', 'Full'
+            return ($capacity / 100, $status ne 'Discharging');
+        }
+    }
     return sub {
         defined(my $charge_full = _slurp_line "$bat/charge_full") or return;
         defined(my $charge_now = _slurp_line "$bat/charge_now") or return;
