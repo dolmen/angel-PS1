d24cb7baf22606a0eca4120c5f6e068eaaee1344
Olivier Mengué <dolmen@cpan.org>
2015-10-22 00:21:15 +0200

    darwin/gen_fetch_battery: rewrite to reduce code and handle more cases

--- lib/AngelPS1/System/darwin.pm
+++ lib/AngelPS1/System/darwin.pm
@@ -21,21 +21,35 @@ sub gen_loadavg
 # If batteries found, returns a closure that returns 2 values:
 # - the battery level as a float between 0 and 1
 # - a boolean; 1 if charging, else discharging
+#
+# For testing:
+#    perl -Ilib -MAngelPS1::System -E 'say for (AngelPS1::System->gen_fetch_battery // exit)->()'
 sub gen_fetch_battery
 {
-    my $no_batteries = split("\n", `pmset -g batt`) - 1;
-    if ($no_batteries < 1) { return; }
-    if ($no_batteries != 1) {
+    # Count lines of pmset -g batt
+    my $lines_count =( )= `pmset -g batt`;
+    return if $?;
+
+    # "Now drawing from 'AC Power'" and no battery info
+    return if $lines_count == 1;
+
+    # FIXME handle "Battery Warning: Early"
+    if ($lines_count > 2) {
         warn "only one battery supported. Patch welcome!"
     }
-    return sub {
-        my @x = split("\t", `pmset -g batt`);
-        my $bat = $x[1];
 
-        $bat =~ /(?<o>\d+)/;
-        my $level = $+{o} / 100;
-        my $status = ($bat =~ / charging/ || 0);
-        return ($level, $status);
+    return sub {
+        my $pmset_batt = `pmset -g batt`;
+
+        # 37%; AC attached; not charging
+        # 8%; charging; 2:46 remaining
+        # 9%; discharging; (no estimate)
+        # 7%; discharging; 0:13 remaining
+        $pmset_batt =~ m/\t([0-9]+)%;.* (dis)?charging/;
+        my $level = $1 / 100;
+        my $charging = ! defined $2;
+
+        return ($level, $charging);
     }
 }
 
