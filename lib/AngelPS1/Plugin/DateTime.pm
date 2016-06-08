9af1cf3ef7fc41055c3e9b73e7de7f17a1fe80be
Olivier Mengué <dolmen@cpan.org>
2016-06-08 09:07:25 +0200

    Plugin::DateTime: add 'Clock' plugin

--- lib/AngelPS1/Plugin/DateTime.pm
+++ lib/AngelPS1/Plugin/DateTime.pm
@@ -1,10 +1,13 @@
+use utf8;
 use strict;
 use warnings;
 
 package AngelPS1::Plugin::DateTime;
 
+use AngelPS1 ();
+
 use Exporter 'import';
-our @EXPORT = qw< Time StrFTime >;
+our @EXPORT_OK = qw< Time StrFTime Clock >;
 
 sub Time ()
 {
@@ -21,6 +24,24 @@ sub StrFTime ($)
 
     require POSIX;
     sub { POSIX::strftime($format, localtime) }
+}
+
+# Unicode characters: "CLOCK FACE" family
+# They are located in the codepages between:
+#     U+1F550 (ONE OCLOCK) and U+1F55B (TWELVE OCLOCK), for the plain hours
+#     U+1F55C (ONE-THIRTY) and U+1F567 (TWELVE-THIRTY), for the thirties
+# Generated with:
+# perl -C -E 'say join("", map {chr(0x1F550+$_)." ".chr(0x1F55C+$_)." "} 0..11)'
+my $CLOCK_FACES = '🕐 🕜 🕑 🕝 🕒 🕞 🕓 🕟 🕔 🕠 🕕 🕡 🕖 🕢 🕗 🕣 🕘 🕤 🕙 🕥 🕚 🕦 🕛 🕧 ';
+
+sub Clock
+{
+    return if ! AngelPS1::_str_allowed($CLOCK_FACES);
+
+    sub {
+        my ($min, $hour) = (localtime)[1,2];
+        substr($CLOCK_FACES, 2*(($hour*60+$min-45)/30%24), 1)
+    }
 }
 
 '$'
